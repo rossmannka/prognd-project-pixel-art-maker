273af289269f6aeae69207044f63876846b06d7e
Karina Roßmann <karina.rossmann@googlemail.com>
2021-03-14 15:53:27 +0100

    Implemented Pixel Art Maker

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
 // Select color input
-// Select size input
+const colorPicker = document.getElementById('colorPicker');
 
 // When size is submitted by the user, call makeGrid()
+const submitButton = document.getElementById('submitButton');
+submitButton.addEventListener('click', makeGrid);
 
-function makeGrid() {
+function makeGrid(event) {
+  // Prevent default user-agent behavior (do NOT modify URL or reload)
+  event.preventDefault();
 
-// Your code goes here!
+  // Get size input
+  const rows = document.getElementById('inputHeight').value;
+  const cells = document.getElementById('inputWidth').value;
 
+  const pixelCanvas = document.getElementById('pixelCanvas');
+  pixelCanvas.innerHTML = '';
+
+  for (let row = 0; row < rows; row++) {
+    const newRow = document.createElement('tr');
+    for (let cell = 0; cell < cells; cell++) {
+      const newCell = document.createElement('td');
+      newCell.addEventListener('click', paintCell);
+      newRow.appendChild(newCell);
+    }
+    pixelCanvas.appendChild(newRow);
+  }
+}
+
+function paintCell(event) {
+  event.target.style.backgroundColor = colorPicker.value;
 }
